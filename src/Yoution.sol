45938a87a6b693d3a8a04b9f84306eab85b18d92
akbarwijayaa <jimsaber02@gmail.com>
2024-10-08 00:54:57 +0700

    fix: main contract

--- src/Yoution.sol
+++ src/Yoution.sol
@@ -1,34 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./reclaim/Reclaim.sol";
 import "./reclaim/Addresses.sol";
 
 contract Yoution is ERC721URIStorage, Ownable {
-    uint256 private tokenId = 1;
-    bool public _hasMinted;
     address public reclaimAddress;
-    string[] public providersHashes;
+    address private ownerContract = 0xAd0938fC6F5e07BCFE96db95f787B0B02497D3bf;
 
+    event LogVerified(bool result);
     event Minted(address indexed owner, uint256 indexed tokenId, string tokenURI);
 
-    constructor(string[] memory _providersHashes) ERC721("Youtube Ownership Proof", "YOP") Ownable(msg.sender) {
-        providersHashes = _providersHashes;
+    constructor() ERC721("Youtube Ownership Proof", "YOP") Ownable(msg.sender) {
         reclaimAddress = Addresses.BASE_SEPOLIA_TESTNET; 
     }
 
-    function isVerifyProof(Reclaim.Proof memory proof) public returns (bool){
-       bool verified = Reclaim(reclaimAddress).verifyProof(proof);
-       return verified;
-    }
+    function mintAccount(Reclaim.Proof memory proof, string memory tokenURI) public {
+        Reclaim(reclaimAddress).verifyProof(proof);
 
-    function mint(Reclaim.Proof memory proof, string memory tokenURI) public returns (uint256) {
-        require(!_hasMinted, "Minting can only be done once");
-        require(isVerifyProof(proof), "Invalid proof");
-        
+        require(proof.signedClaim.claim.owner == ownerContract, "Owner not trusted!");
+        require(bytes(proof.claimInfo.context).length > 0, "Context cannot empty!");
+
+        bytes32 hashTokenId = keccak256(abi.encodePacked(proof.claimInfo.context));
+        uint256 tokenId = uint256(hashTokenId);
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
